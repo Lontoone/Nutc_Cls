e7a1df01eb57cb09f4094dafebf7d1127e2b0c9c
Lontoone <news2000tw@gmail.com>
2022-10-31 10:11:00 +0800

    sql ok

--- web/src/Components/DbHelper.js
+++ web/src/Components/DbHelper.js
@@ -7,42 +7,59 @@ import fs from "fs";
 const dblink =
   "https://github.com/Lontoone/Nutc_Cls/blob/master/Datas/cls.db?raw=true";
 const localLink = "D:/Work/NUTC_Cls/web/src/cls.db";
+const relLink = process.env.PUBLIC_URL + "/cls.db";
 const tempLink =
   "https://nutcedutw-my.sharepoint.com/:u:/g/personal/s1110534008_nutc_edu_tw/ER6xYCSwdOVCtCNUWAmII24BuBi7N-zCFUHF7MMweQj9SQ?e=A8CtyE";
+
+
+
+function loadBinaryFile(path, success) {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", path, true);
+  xhr.responseType = "arraybuffer";
+  xhr.onload = function () {
+    var data = new Uint8Array(xhr.response);
+    var arr = new Array();
+    for (var i = 0; i != data.length; ++i) arr[i] = String.fromCharCode(data[i]);
+    success(arr.join(""));
+  };
+  xhr.send();
+};
 async function GetDb() {
   const SQL = await initSqlJs({ locateFile: () => sqlWasm });
-  /*
+
   //const db = new SQL.Database("D:/Work/NUTC_Cls/web/src/cls.db");
-  var data = fs.readFileSync('D:/Work/NUTC_Cls/web/src/cls.db');
-  var db = new SQL.Database(data);*/
-  /*
-  const sqlPromise = initSqlJs({ locateFile: () => sqlWasm });
-  const dataPromise = fetch(dblink).then((res) =>
-    res.arrayBuffer()
-  );
-  const [SQL, buf] = await Promise.all([sqlPromise, dataPromise]);
-  const db = new SQL.Database(new Uint8Array(buf));
-  */
-  /*
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", tempLink, true);
-  xhr.responseType = "arraybuffer";
 
-  console.log(xhr);
-  xhr.onload = function (e) {
-    var uInt8Array = new Uint8Array(this.response);
-    var db = new SQL.Database(uInt8Array);
-    var contents = db.exec("SELECT * FROM cls");
-    // contents is now [{columns:['col1','col2',...], values:[[first row], [second row], ...]}]
-    console.log(contents);
-  };
-  //console.log(db);*/
-  let reader = new FileReader();
-  reader.onload = function (e) {
-    let arrayBuffer = new Uint8Array(reader.result);
-    console.log(arrayBuffer);
-  };
+  /*
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = "arraybuffer";
+    console.log(relLink);
+    console.log(xhr);
+    xhr.onreadystatechange = function (e) {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200 || xhr.status == 0) {
+          console.log(e);
+          var uInt8Array = new Uint8Array(this.response);
+          var db = new SQL.Database(uInt8Array);
+          //var contents = db.exec("SELECT * FROM cls");
+          // contents is now [{columns:['col1','col2',...], values:[[first row], [second row], ...]}]
+          console.log(db);
+  
+        }
+      }
+  
+    };
+    
+    xhr.open("GET", relLink, true);
+    xhr.send();
+    */
 
-  reader.readAsArrayBuffer("../cls.db");
+  loadBinaryFile(relLink, function (data) {
+    var sqldb = new SQL.Database(data);
+    // Database is ready
+    var res = sqldb.exec("SELECT * FROM cls");
+    console.log(res);
+  });
 }
+
 export default GetDb;

40628984cdfcebd6c922ca66be92d4ffcf091127
cingjia <news2000tw@gmail.com>
2022-11-02 22:29:20 +0800

    scrap data Update at 2022-11-02

--- web/public/Datas/RunUpdate.py
+++ web/public/Datas/RunUpdate.py
@@ -1,14 +1,14 @@
 import os
 import subprocess
 from datetime import date
-from pathlib import Path
 cwd =os.getcwd()
 par_cwd= os.path.dirname (os.path.realpath(__file__))
-print(par_cwd)
+git_cwd= cwd+"\web"
+print(cwd)
+print(git_cwd)
+
 today = date.today()
 subprocess.run(["python","scrap.py"], cwd=f"{par_cwd}").stdout
-subprocess.run(["git","add","."], cwd=f"{par_cwd}")
-subprocess.run(["git","commit","-m",f"scrap data Update at {today}"], cwd=f"{par_cwd}")
-subprocess.run(["git","push"], cwd=f"{par_cwd}")
-
-'''subprocess.run(["pause"])'''
+subprocess.run(["git","add","."], cwd=f"{git_cwd}")
+subprocess.run(["git","commit","-m",f"scrap data Update at {today}"], cwd=f"{git_cwd}")
+subprocess.run(["git","push"], cwd=f"{git_cwd}")
